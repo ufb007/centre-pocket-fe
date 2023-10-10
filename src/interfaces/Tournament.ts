38a7b12603aaec18743349ba3ded170990cde439
Ufuk Bozdemir <u.bozdemir@sentrybay.com>
2023-10-10 17:15:42 +0100

    Working on tournaments section.

---
+++ src/interfaces/Tournament.ts
@@ -0,0 +1,20 @@
+export interface TournamentInterface {
+    id: number;
+    uuid: string;
+    name: string;
+    description: string;
+    game_type: '9ball' | '8ball' | '10ball' | 'straight';
+    type: 'single' | 'double' | 'round_robin';
+    max_players: 8 | 16 | 32 | 64 | 128;
+    race_to: number;
+    cover_image: string;
+    start_date: Date;
+    status: 'upcoming' | 'active' | 'finished';
+    createdAt: Date;
+    tournament_players: {
+        id: number;
+        playing: 'pending' | 'yes' | 'no';
+        paid: 0 | 1;
+        createdAt: Date;
+    }[]
+}
